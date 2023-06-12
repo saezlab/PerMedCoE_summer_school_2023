d398a24a72a7c11b503f78f9d4c37c7deb6693bc
Pablo R. Mier <pablo.rodriguez.mier@gmail.com>
2023-06-12 12:19:32 +0200

    Update install script

--- install.sh
+++ install.sh
@@ -1,6 +1,24 @@
 #!/usr/bin/env bash -l
 set -euo pipefail
 
+# Function to check if micromamba is installed
+function check_micromamba() {
+    if ! command -v micromamba &> /dev/null; then
+        echo "Micromamba not found, installing..."
+		curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+		~/.local/bin/micromamba shell init -p "$HOME/micromamba"
+		echo 'export MAMBA_ROOT_PREFIX="$HOME/micromamba"' >> ~/.bashrc
+        echo 'export PATH="$MAMBA_ROOT_PREFIX/bin:$PATH"' >> ~/.bashrc
+        source ~/.bashrc
+    else
+        echo "Micromamba is already installed"
+    fi
+}
+
+# Call the function to check micromamba
+check_micromamba
+
+# Continue with the script
 micromamba create -y --name saezlab python=3.9 pandas=1.5.3 numpy=1.24.3 jupyter=1.0.0 python-graphviz=0.20.1 matplotlib=3.7.1 seaborn=0.12.2 cvxpy=1.3.1 pyscipopt=4.3.0
 micromamba activate saezlab
 pip install git+https://github.com/saezlab/decoupler-py.git@4b3978f

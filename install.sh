bce7886049d9df1278068c67e86b02c2e35b727e
Pablo R. Mier <pablo.rodriguez.mier@gmail.com>
2023-06-12 13:52:24 +0200

    Add missing channel in install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ function check_micromamba() {
 check_micromamba
 
 # Continue with the script
-micromamba create -y --name saezlab python=3.9 pandas=1.5.3 numpy=1.24.3 jupyter=1.0.0 python-graphviz=0.20.1 matplotlib=3.7.1 seaborn=0.12.2 cvxpy=1.3.1 pyscipopt=4.3.0
+micromamba create -y --name saezlab python=3.9 pandas=1.5.3 numpy=1.24.3 jupyter=1.0.0 python-graphviz=0.20.1 matplotlib=3.7.1 seaborn=0.12.2 cvxpy=1.3.1 pyscipopt=4.3.0 -c conda-forge
 micromamba activate saezlab
 pip install git+https://github.com/saezlab/decoupler-py.git@4b3978f
 pip install git+https://github.com/saezlab/omnipath.git@v1.0.7
